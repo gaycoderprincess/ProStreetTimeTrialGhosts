1dbb23905206eab480ecf85fbf02b3f6327787d9
💖 Chloe 💖 <gaycoderprincess@proton.me>
2026-02-12 11:15:25 +0100

    fix ghost hide

--- hooks/carrender.h
+++ hooks/carrender.h
@@ -1,30 +1,28 @@
-void HideGhostCar(NyaMat4x4* carMatrix) {
+bool HideGhostCar(NyaMat4x4* carMatrix) {
 	auto car = GetClosestActiveVehicle(RenderToWorldCoords(carMatrix->p));
-	if (!car) return;
-	if (car == GetLocalPlayerVehicle()) return;
-	if (car->IsStaging()) return;
-	if (car->GetDriverClass() == DRIVER_TRAFFIC || car->GetDriverClass() == DRIVER_COP) return;
+	if (!car) return false;
+	if (car == GetLocalPlayerVehicle()) return false;
+	if (car->IsStaging()) return false;
+	if (car->GetDriverClass() == DRIVER_TRAFFIC || car->GetDriverClass() == DRIVER_COP) return false;
 
 	bool hide = nGhostVisuals == GHOST_HIDE;
 	if (!hide) {
 		auto playerCar = GetLocalPlayerVehicle();
-		if (!playerCar) return;
+		if (!playerCar) return false;
 
 		auto dist = (*playerCar->GetPosition() - *car->GetPosition()).length();
 		if (dist < 8) hide = true;
 	}
-
-	if (!hide) return;
-
-	// hacky solution!! it works but checking some CarRenderInfo ptr against the player and disabling DrawCars would be way better
-	carMatrix->p = {0,0,0};
+	return hide;
 }
 
 auto CarGetVisibleStateOrig = (int(__thiscall*)(eView*, const bVector3*, const bVector3*, bMatrix4*))nullptr;
 int __thiscall CarGetVisibleStateHooked(eView* a1, const bVector3* a2, const bVector3* a3, bMatrix4* a4) {
 	auto carMatrix = (NyaMat4x4*)a4;
 	if (TheGameFlowManager.CurrentGameFlowState == GAMEFLOW_STATE_RACING && !IsInNIS() && (nGhostVisuals == GHOST_HIDE || nGhostVisuals == GHOST_HIDE_NEARBY)) {
-		HideGhostCar(carMatrix);
+		if (HideGhostCar(carMatrix)) {
+			return 0;
+		}
 	}
 	return CarGetVisibleStateOrig(a1, a2, a3, a4);
 }
